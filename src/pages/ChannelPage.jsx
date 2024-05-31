60e9e6b65d32c32c154a5e686831e677d73fcbf2
최선화 <161446039+sunhew@users.noreply.github.com>
2024-05-31 12:00:13 +0900

    1

--- src/pages/ChannelPage.jsx
+++ src/pages/ChannelPage.jsx
@@ -18,12 +18,12 @@ const ChannelPage = () => {
     useEffect(() => {
         const fetchResults = async () => {
             try {
-                const detail = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet,brandingSettings,statistics&id=${channelID}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
+                const detail = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet,brandingSettings,statistics&id=${channelID}&key=${process.env.REACT_APP_YOUTUBE_API_KEY_TO}`)
                 const detailData = await detail.json();
                 setChannelDetail(detailData.items[0]);
                 console.log(detailData);
 
-                const video = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&order=date&maxResults=48&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
+                const video = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&order=date&maxResults=48&key=${process.env.REACT_APP_YOUTUBE_API_KEY_TO}`)
                 const videoData = await video.json();
                 setChannelVideo(videoData.items);
                 setNextPageToken(videoData.nextPageToken);
@@ -40,7 +40,7 @@ const ChannelPage = () => {
 
     const loadMoreVideos = async () => {
         try {
-            const nextVideo = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&order=date&maxResults=48&pageToken=${nextPageToken}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
+            const nextVideo = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&order=date&maxResults=48&pageToken=${nextPageToken}&key=${process.env.REACT_APP_YOUTUBE_API_KEY_TO}`)
             const videoData = await nextVideo.json();
             setChannelVideo(prevVideos => [...prevVideos, ...videoData.items]);
             setNextPageToken(videoData.nextPageToken);
